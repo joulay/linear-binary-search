4de2083ad88919ce1ba35a9a3582d079cb84f8fa
Julie K <julieskim160@gmail.com>
2018-05-23 17:38:47 -0700

    fix the export bug

--- src/form.js
+++ src/form.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Form extends React {
+export default class Form extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -11,20 +11,39 @@ export default class Form extends React {
                 28, 13, 17, 69, 90, 1, 6, 7, 64, 43, 9, 73, 80, 98, 46, 27, 22, 87, 
                 49, 83, 6, 39, 42, 51, 54, 84, 34, 53, 78, 40, 14, 5],
             input: '',
-            count: ''
-        } 
-
-    }
-
+            response: ''
+            
+        }
+    } 
     render() {
-    return
-        <div>
-            <form>
-                <label>Search</label>
-                <input type="text" name="input"/>
-                <button type="submit" onClick={()=>this.linear}></button>
-            </form>
-        </div>
+
+        function linear(input){
+            let count = 0;
+            for (let i = 0; i < this.state.data.length; i++) {
+                count++;
+                if (this.state.data[i] === input) {
+                    return this.setState({
+                        input: `${input}`,
+                        response: `${count} tries`
+                    })
+                }
+            }
+        }
+
+        // binary()
+
+  
+ 
+        return (
+            <div>
+                <form>
+                    <label>Search</label>
+                    <input type="text" name="input"/>
+                    <button type="submit" onClick={()=>this.linear}>Linear</button>
+                    <button type="submit" onClick={()=>this.binary}>Binary</button>
+                </form>
+            </div>
+        )
     }
 
 }
